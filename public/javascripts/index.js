34d8cf05b06ed11d9dd2ecbb7a9965d5d6a32f71
sergey.shablenko <shablenko@sednor.net>
2020-07-30 17:02:29 +0300

    implement invoice page, improve ui, minor fixes

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,49 +2,8 @@
 
 const COOKIE_NAME = "settings";
 
-// ============== Helpers ===============
-const setCookie = (key, value, expiry = 24) => {
-    const expires = new Date();
-    expires.setTime(expires.getTime() + (expiry * 24 * 60 * 60 * 1000));
-    document.cookie = `${key}=${value};expires=${expires.toISOString()};samesite=strict`;
-};
-
-const getCookie = (key) => {
-    if (document.cookie) {
-        const cookie = document.cookie.split("; ").find(x => x.startsWith(key));
-        return cookie && JSON.parse(cookie.split(/=(.+)/)[1]);
-    }
-    return null;
-};
-
-const getPaymentSDKInstance = (deviceId) => {
-    const settings = getCookie(COOKIE_NAME) || {};
-
-    return new window.XcooBee.XcooBeePaymentSDK({
-        campaignId: settings.campaign_id,
-        formId: settings.campaign_form_id,
-        deviceId,
-    });
-};
-
-const getSDKInstance = () => {
-    const settings = getCookie(COOKIE_NAME) || {};
-
-    const config = new XcooBee.sdk.Config({
-        apiUrlRoot: settings.api_url,
-        apiKey: settings.api_key,
-        apiSecret: settings.api_secret,
-        campaignId: settings.campaign_id,
-        pgpPassword: settings.pgp_password,
-        pgpSecret: settings.pgp_key,
-    });
-    return new XcooBee.sdk.Sdk(config);
-};
-
-const stringToCamelCase = str => str.replace(/(\_\w)/g, m => m[1].toUpperCase());
-
-
 $(() => {
+    // ============== Alerts ===============
     const alertBlock = $("#alert_block").hide();
 
     const handleError = (message) => {
@@ -57,21 +16,72 @@ $(() => {
         alertBlock.addClass("alert-success").removeClass("alert-danger").show();
     };
 
+    alertBlock.find($(".close")).click(() => alertBlock.hide());
+
+    // ============== Helpers ===============
+    const setCookie = (key, value, expiry = 24) => {
+        const expires = new Date();
+        expires.setTime(expires.getTime() + (expiry * 24 * 60 * 60 * 1000));
+        document.cookie = `${key}=${value};expires=${expires.toISOString()};samesite=strict`;
+    };
+
+    const getCookie = (key) => {
+        if (document.cookie) {
+            const cookie = document.cookie.split("; ").find(x => x.startsWith(key));
+            return cookie && JSON.parse(cookie.split(/=(.+)/)[1]);
+        }
+        return null;
+    };
+
+    const getPaymentSDKInstance = (deviceId) => {
+        const settings = getCookie(COOKIE_NAME) || {};
+
+        return new window.XcooBee.XcooBeePaymentSDK({
+            campaignId: settings.campaign_id,
+            formId: settings.campaign_form_id,
+            deviceId,
+        });
+    };
+
+    const getSDKInstance = () => {
+        const settings = getCookie(COOKIE_NAME) || {};
+
+        try {
+            const config = new window.XcooBee.sdk.Config({
+                apiUrlRoot: "https://api.xcoobee.net",
+                apiKey: settings.api_key,
+                apiSecret: settings.api_secret,
+                campaignId: settings.campaign_id,
+                pgpPassword: settings.pgp_password,
+                pgpSecret: settings.pgp_key,
+            });
+            return new window.XcooBee.sdk.Sdk(config);
+        } catch (e) {
+            handleError(e.message);
+            throw e;
+        }
+    };
+
+    const stringToCamelCase = str => str.replace(/(\_\w)/g, m => m[1].toUpperCase());
+
+    $("#xcoobee_api_result").hide();
+
     const stopPoller = () => {
-        clearInterval(interval);
-        $('#start_poll').removeClass("btn-default").addClass("btn-primary");
-        $('#stop_poll').removeClass("btn-primary").addClass("btn-default");
+        clearTimeout(timer);
+        timer = null;
+        $("#start_poll").removeClass("btn-primary").addClass("btn-outline-primary");
+        $("#stop_poll").removeClass("btn-outline-primary").addClass("btn-light");
         $("button").prop("disabled", false);
-        $(".xcoobee_clock").html("<p>waiting for poll</p>");
+        $(".xcoobee_clock").html("<div>waiting for poll</div>");
         $(".xcoobee_loader").hide();
-    }
+    };
 
     const displayPollerResponse = () => {
-        $("button").prop("disabled", true);
+        $("#xcoobee_api_result").show();
         $(".xcoobee_loader").show();
         const xcooBeeSdk = getSDKInstance();
 
-        xcooBeeSdk.system.getEvents()
+        return xcooBeeSdk.system.getEvents()
             .then((response) => {
                 if (response.result
                     && response.result
@@ -83,7 +93,7 @@ $(() => {
                         html = `
                             <tr>
                                 <td>${stringToCamelCase(elm.event_type)}</td>
-                                <td><pre>${elm.payload}</pre></td>
+                                <td><pre>${JSON.stringify(elm.payload)}</pre></td>
                                 <td>${elm.reference_type}</td>
                                 <td>${elm.date_c}</td>
                             </tr>
@@ -94,7 +104,6 @@ $(() => {
                     $("#xcoobee_api_result tbody").html("no running events");
                 }
                 $(".xcoobee_loader").hide();
-                $("button").prop("disabled", false);
             })
             .catch((err) => {
                 handleError(err.error.message);
@@ -102,17 +111,18 @@ $(() => {
             });
     };
 
-    const pollServer = (serverPollInterval) => {
-        let count = parseInt(serverPollInterval, 10);
+    const pollServer = (serverPollInterval, countDown) => {
+        let count = countDown === undefined ? parseInt(serverPollInterval, 10) : countDown;
 
-        interval = setInterval(function () {
+        timer = setTimeout(function () {
             count = count > 0 ? count - 1 : count;
-            $("button").prop("disabled", true);
-            $(".xcoobee_clock").html(`<p>next poll in <span>${count}</span> s</p>`);
+            $(".xcoobee_clock").html(`<div>next poll in <span>${count}</span> s</div>`);
 
             if (count === 0) {
                 count = parseInt(serverPollInterval, 10);
-                displayPollerResponse();
+                displayPollerResponse().then(() => timer && pollServer(serverPollInterval)).catch(() => timer && pollServer(serverPollInterval));
+            } else if (timer) {
+                pollServer(serverPollInterval, count);
             }
         }, 1000);
     };
@@ -120,7 +130,7 @@ $(() => {
     // ============= Menu ==================
     const current = location.pathname;
     $(".navbar-nav li a").each((i, item) => {
-        if ($(item).attr("href") === current){
+        if ($(item).attr("href") === current) {
             $(item).addClass("active");
         }
     });
@@ -132,29 +142,32 @@ $(() => {
     });
     $("form.settings-form").submit((e) => {
         e.preventDefault();
-        alertBlock.hide();
     });
     $("button.submit-settings-form").click(function () {
-        alertBlock.hide();
         let existingCookies = getCookie(COOKIE_NAME) || {};
-        const formData = $(this.form).serializeArray()
-        if (formData.every(elem => elem.value)) {
-            formData.forEach((item) => {
-                existingCookies = Object.assign(existingCookies, {
-                    [item.name]: item.value,
-                });
+        const formData = $(this.form).serializeArray();
+
+        formData.forEach((item) => {
+            existingCookies = Object.assign(existingCookies, {
+                [item.name]: item.value,
             });
-        }
+        });
         if (Object.keys(existingCookies).length) {
             setCookie(COOKIE_NAME, JSON.stringify(existingCookies));
         }
+        handleSuccess("Settings saved");
     });
     $("#test_settings").click(() => {
+        $("button.submit-settings-form").click();
+        alertBlock.hide();
+
         const xcooBeeSdk = getSDKInstance();
 
+        $("#test_settings").prop("disabled", true);
         xcooBeeSdk.system.ping()
             .then(() => handleSuccess("Everything's fine"))
-            .catch(err => handleError(err.error.message));
+            .catch(err => handleError(err.error.message))
+            .then(() => $("#test_settings").prop("disabled", false));
     });
 
     // ============= Shopping cart page ===========
@@ -167,20 +180,20 @@ $(() => {
         const price = parseFloat(productCart.find(".product_price").first().html());
         const name = productCart.find(".product_name").first().html();
 
-        const link = xcoobeeSDK.createMultiSelectWithCostUrl({
+        const link = xcoobeeSDK.createSingleSelectUrl({
             amount: price,
             reference: name,
-            options: []
+            options: ["M", "L", "XL"],
         });
 
         $("#shopping_cart").attr("src", link);
     });
 
     // ============ Poller page ===================
-    let interval;
+    let timer;
 
     $.validator.addMethod("pollinterval", function (value, element) {
-        const v = $('input[name="' + element.name + '"]').val();
+        const v = $("input[name=\"" + element.name + "\"]").val();
         return (v == 0 || (v >= 60 && v <= 600));
     }, "Enter interval in seconds 0 or min. 60 - max 600");
 
@@ -190,18 +203,20 @@ $(() => {
             submitHandler: function () {
                 alertBlock.hide();
                 const submitButtonName = $(this.submitButton).attr("name");
-                const pollInterval = $("#xcoobee_poll_interval").val();
+                const pollInterval = +$("#xcoobee_poll_interval").val() || 0;
                 if (submitButtonName === "start_poll" && pollInterval !== 0) {
-                    $("#start_poll").removeClass("btn-primary").addClass("btn-default");
-                    $("#stop_poll").removeClass("btn-default").addClass("btn-primary");
+                    $("#start_poll").removeClass("btn-outline-primary").addClass("btn-primary").prop("disabled", true);
+                    $("#stop_poll").removeClass("btn-light").addClass("btn-outline-primary");
+                    $("#manual_poll").prop("disabled", true);
                     displayPollerResponse();
                     pollServer(pollInterval);
                 }
                 if (submitButtonName === "manual_poll" || pollInterval === 0) {
-                    displayPollerResponse();
+                    $("button").prop("disabled", true);
+                    displayPollerResponse().then(() => stopPoller());
                 }
                 return false;
-            }
+            },
         });
 
     $("#stop_poll").on("click", () => {
@@ -212,8 +227,8 @@ $(() => {
     // ============= Payments page ===========
     $("#payment_form").submit(e => e.preventDefault());
     $("#request_payment").click(function () {
-        const properties = {}
-        const formData = $(this.form).serializeArray()
+        const properties = {};
+        const formData = $(this.form).serializeArray();
         formData.forEach((item) => {
             properties[item.name] = item.value;
         });
@@ -226,5 +241,29 @@ $(() => {
         });
 
         $("#payment_cycle").attr("src", link);
+        setTimeout(() => pollServer(2), 25000);
     });
+
+    // ============= Invoice page ===========
+    $("#invoice_form")
+        .submit(e => e.preventDefault())
+        .validate({
+            submitHandler: function () {
+                const settings = getCookie(COOKIE_NAME) || {};
+
+                if (!settings.campaign_id) {
+                    alertBlock.show();
+                    handleError("Campaign ID is required");
+                    return;
+                }
+
+                const properties = {};
+                const formData = $("#invoice_form").serializeArray();
+                formData.forEach((item) => {
+                    properties[item.name] = item.value;
+                });
+
+                window.open(`/generate-invoice?ref=${properties.payment_reference}&amount=${properties.payment_amount}&campaignId=${settings.campaign_id}`, "_blank");
+            },
+        });
 });
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 "use strict";
 
+const os = require("os");
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
+const stream = require("stream");
+const Handlebars = require("handlebars");
+const wkhtmltopdf = require("wkhtmltopdf");
+const { NodeXcooBeePaymentSDK } = require("@xcoobee/payment-sdk");
 
 const router = express.Router();
 
@@ -17,6 +24,27 @@ router
     .get("/payment", (req, res) => {
         res.render("payment", { title: "Full Payment Cycle" });
     })
+    .get("/invoice", (req, res) => {
+        res.render("invoice", { title: "Invoice Creator" });
+    })
+    .get("/generate-invoice", (req, res, next) => {
+        const sdk = new NodeXcooBeePaymentSDK(req.query);
+        const params = { amount: req.query.amount, reference: req.query.ref || "Order" };
+
+        sdk
+            .createPayQr(params)
+            .then(qr => {
+                const template = fs.readFileSync(path.resolve(__dirname, "../templates/invoice.hbs"), "utf8");
+                const tmpl = Handlebars.compile(template);
+                const tmp = tmpl({ ...params, qr, formUrl: sdk.createPayUrl(params) });
+                const filePath = path.resolve(os.tmpdir(), "test.pdf");
+
+                wkhtmltopdf(tmp, { output: filePath }, () => {
+                    res.download(filePath);
+                });
+            })
+            .catch(err => next(err));
+    })
     .get("/shopping_cart", (req, res) => {
         res.render("shoppingCart", {
             title: "Shopping Cart",
@@ -25,11 +53,6 @@ router
                     name: "Beanie",
                     price: 18.95,
                     image: "/images/beanie.jpg",
-                },
-                {
-                    name: "Belt",
-                    price: 9.95,
-                    image: "/images/belt.jpg",
                 },
                 {
                     name: "Gloves",
